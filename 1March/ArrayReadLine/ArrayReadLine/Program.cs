e53d2192004d8315c9ae448aed8ef16488254b4a
elnurxo <elnurykh@code.edu.az>
2023-03-01 09:44:28 +0400

    console readline practise, try parse

---
+++ 1March/ArrayReadLine/ArrayReadLine/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ArrayReadLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            string sizeStr;
+
+            do
+            {
+                Console.Write("Enter size of array: ");
+                sizeStr = Console.ReadLine();
+            } while (!int.TryParse(sizeStr,out size));
+
+            size = Convert.ToInt32(sizeStr);
+
+            //initializing empty array
+            double[] points = new double[size];
+
+            for (int i = 0; i < size; i++) //points.Length
+            {
+                Console.Write($"{i+1}. enter point: ");
+                string pointStr = Console.ReadLine();
+                double point = Convert.ToDouble(pointStr);
+                points[i] = point;
+
+                //points[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("-------------------------------");
+            foreach (double point in points)
+            {
+                Console.WriteLine(point);
+            }
+        }
+    }
+}
